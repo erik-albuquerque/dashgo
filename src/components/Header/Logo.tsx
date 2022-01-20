0c2d3c40a4f5e7fe72f0dbbdccf1cf7578661827
Erik Albuquerque <eriksilv.77@gmail.com>
2022-01-20 17:04:22 -0300

    feat: add responsive header

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,7 +1,12 @@
 import { Text } from "@chakra-ui/react";
 export function Logo() {
   return (
-    <Text w="64" fontSize="3xl" fontWeight="bold" letterSpacing="tight">
+    <Text
+      w="64"
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+    >
       dashgo
       <Text as="span" color="pink.500">
         .
